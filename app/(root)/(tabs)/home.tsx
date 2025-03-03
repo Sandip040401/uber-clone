e0aaec68a6fcbdee43b1be75ccce989d3b53546e
Sandip040401 <iemsandip@gmail.com>
2025-03-04 00:23:16 +0530

    update ride card

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,12 @@
 import RideCard from '@/components/RideCard'
+import { icons, images } from '@/constants'
 import { useUser } from '@clerk/clerk-expo'
-import { FlatList, Text } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Text, Touchable, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Home() {
   const { user } = useUser()
+  const loading = true
   const recentRides= [
     {
         "ride_id": "1",
@@ -103,11 +105,44 @@ export default function Home() {
         }
     }
 ]
-
+  const handleSignOut = () => {
+    // signOut()
+  }
   return (
     <SafeAreaView className='bg-general-500'>
       <FlatList data={recentRides?.slice(0,5)}
+      // <FlatList data={[]}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className='px-5'
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className='flex flex-col items-center justify-center'>
+            {!loading ? (
+              <>
+                <Image source={images.noResult} className='w-40 h-40' alt='No Recent Rides Found' resizeMode='contain'/>
+                <Text className='text-sm'>No Recent Rides Found</Text>
+              </>
+            ):(
+              <ActivityIndicator size="small" color="#000"/>
+            )}
+          </View>
+  )}
+        ListHeaderComponent={() => (
+          <>
+            <View className='flex flex-row items-center justify-between my-5'>
+              <Text className='text-2xl capitalize font-JakartaExtraBold'>
+                Welcome{", "} {user?.firstName || user?.emailAddresses[0].emailAddress.split('@')[0]}{" "}
+              </Text>
+              <TouchableOpacity onPress={handleSignOut} className='justify-center items-center w-10 h-10 rounded-full bg-white'>
+                <Image source={icons.out} className='w-4 h-4'/>
+              </TouchableOpacity>
+            </View>
+            
+          </>
+  )}
       />
 
     </SafeAreaView>
